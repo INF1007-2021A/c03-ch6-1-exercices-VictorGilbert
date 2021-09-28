9171dfe5ac48decb7d06c7519eb012cf2da58bde
VictorGilbert <zevictos@gmail.com>
2021-09-28 12:08:47 -0400

    miaw

--- exercice.py
+++ exercice.py
@@ -5,47 +5,95 @@
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
-
-    return []
+        boucle= True
+        values=[]
+        while boucle:
+            nb=input("LES VALEURS?!!!!!(n to stop)")
+            if nb=="n":
+                boucle=False
+            else:
+                values.append(float(nb))
+    return values.sort()
 
 
 def anagrams(words: list = None) -> bool:
+    anagram=False
+    alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     if words is None:
         # TODO: demander les mots ici
-        pass
-
-    return False
+        words1=input("La phrase1?!!!!!")
+        words2=input("La phrase2??")
+        rep=True
+        if False not in [True if words1.count(lettres)==words2.count(lettres) else False for lettres in alphabet ]:
+            anagram=True
+    return anagram
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    pas_doublons=False
+    for i in range(len(items)):
+        for j in range(len(items)):
+            if i==j:
+                pass
+            elif items[i]==items[j]:
+                pas_doublons= True
+    return pas_doublons
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    élèves=list(student_grades.keys())
+    moyenne_la_plus_grande=0
+    nom=""
+    for i in range(len(élèves)):
+        if moyenne_la_plus_grande<(sum(student_grades[élèves[i]])%len(student_grades[élèves[i]])):
+            moyenne_la_plus_grande=sum(student_grades[élèves[i]])%len(student_grades[élèves[i]])
+            nom=élèves[i]
+    rep={}
+    rep[moyenne_la_plus_grande]=nom
+    return rep
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    lettres={}
+    rep={}
+    for i in range(26):
+        if sentence.count(alphabet[i])>5:
+            lettres[alphabet[i]]=sentence.count(alphabet[i])
 
-    return {}
+    return sorted(lettres.items(), key=lambda x:-x[-1])
 
-
+recipes={}
+global recipe
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    boucle=True
+    while boucle:
+        ingredients=[]
+        nb=input("ingrédients (n to stop)")
+        if nb=="n":
+            boucle=False
+        else:
+            ingredients.append(nb)
+        num=len(recipes)
+        recipes[num]=ingredients
+        
+    return num
+    
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    if ingredients in recipes.keys():
+        return print(recipes[ingredients])
+    else:
+        return print("Connai pas la recette frere")
 
 
 def main() -> None:
-    print(f"On essaie d'ordonner les valeurs...")
     order()
 
     print(f"On vérifie les anagrammes...")
